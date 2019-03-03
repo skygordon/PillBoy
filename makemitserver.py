6592616dee70fd23a1d7d9da7706f9dc57b4d154
Skylar Gordon <47067071+skygordon@users.noreply.github.com>
2019-03-03 09:07:31 -0500

    Delete makemitserver.py

--- makemitserver.py
+++
@@ -1,7 +0,0 @@
-import requests
-from requests.auth import HTTPBasicAuth
-
-def request_handler(request):
-    message = request['form']['text']
-    r = requests.post('https://api.twilio.com/2010-04-01/Accounts/AC3e032d8f15c87c6fae7b7a92528be27f/Messages.json',auth=HTTPBasicAuth('AC3e032d8f15c87c6fae7b7a92528be27f', 'cc1aa85f5b554b2e15ea0585e0375f1c'), data = {'To':'+18144943971','MessagingServiceSid':'MG2dba0ee552919a30d9bb6cf575de853e','Body':f'{message}'})
-    return(r)
